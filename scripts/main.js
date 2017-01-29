65bc1853d6fe10bd1433e746dd29b470950013fa
Cont Activ <dancozianu@yahoo.com>
2017-01-29 19:14:51 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,14 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.jpg') {
+    if(mySrc === 'images/contactiv.jpg') {
       myImage.setAttribute ('src','images/firefox2.jpg');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.jpg');
+      myImage.setAttribute ('src','images/contactiv.jpg');
     }
 }
 
-var myButton = document.querySelector('button');
+/*var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
@@ -27,4 +27,4 @@ if(!localStorage.getItem('name')) {
 
 myButton.onclick = function() {
   setUserName();
-}
+}*/
