fbe7724a373dfb601d0ae39d753dc46fb14016a8
Cont Activ <dancozianu@yahoo.com>
2017-02-20 14:29:23 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    $(".pull-me").click(function(){
-        $(".panel").slideToggle('slow')    
-    }); 
-});
-
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -14,38 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/contactiv.jpg');
     }
 }
-
-$(document).ready(function(){
-    $(".pull-me").click(function(){
-        $(".panel").slideToggle('slow')    
-    }); 
-});
-
-/* $(document).ready(function() {
-    $('li').mouseenter(function() {
-        $('li').fadeTo('fast',1);
-    });
-    $('li').mouseleave(function() {
-        $('li').fadeTo('fast',0.5);
-    });
-});
-
-/*var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Va rugam introduceti numele dumneavoastra');
-  localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Bine ati venit pe website-ul Cont Activ, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Bine ati venit pe website-ul Cont Activ, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}*/
